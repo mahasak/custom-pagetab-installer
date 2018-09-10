ac1d861c290d98d27df041875486378cc729fce6
Mahasak Pijittum <mahasak@gmail.com>
2018-09-10 22:16:55 +0800

    remove unused code

--- facebook-tab/src/App.js
+++ facebook-tab/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import config from '../config';
+import config from './config';
 import './App.css';
 
 const App = () => (
@@ -13,28 +13,6 @@ const App = () => (
 );
 
 class Install extends Component {
-  statusChangeCallback = (response) => {
-    console.log('statusChangeCallback');
-    console.log(response);
-    // The response object is returned with a status field that lets the
-    // app know the current login status of the person.
-    // Full docs on the response object can be found in the documentation
-    // for FB.getLoginStatus().
-    if (response.status === 'connected') {
-      window.FB.ui({
-        method: 'pagetab',
-        display: 'iframe',
-        redirect: config.redirect_uri
-      }, function(response){
-        console.log(response);
-      });
-    } else {
-      // The person is not logged into your app or we are unable to tell.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
-    }
-  }
-
   componentDidMount() {
     window.fbAsyncInit = function() {
       window.FB.init({
@@ -45,12 +23,6 @@ class Install extends Component {
       });
 
       window.FB.getLoginStatus(function(response) {
-        console.log('statusChangeCallback');
-        console.log(response);
-        // The response object is returned with a status field that lets the
-        // app know the current login status of the person.
-        // Full docs on the response object can be found in the documentation
-        // for FB.getLoginStatus().
         if (response.status === 'connected') {
           window.FB.ui({
             method: 'pagetab',
@@ -60,12 +32,10 @@ class Install extends Component {
             console.log(response);
           });
         } else {
-          // The person is not logged into your app or we are unable to tell.
           document.getElementById('status').innerHTML = 'Please log ' +
-            'into this app.';
+            'into to faebook.';
         }
       });
-      
     };
       
     (function(d, s, id){
