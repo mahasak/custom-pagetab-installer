f3b42f44bf2a002608e902b2bcc8163311bdcab9
Mahasak Pijittum <mahasak@gmail.com>
2018-09-10 20:25:09 +0800

    add basic route

--- facebook-tab/src/App.js
+++ facebook-tab/src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+const App = () => (
+  <Router>
+    <div>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+    </div>
+  </Router>
+);
+
+class App2 extends Component {
   render() {
     return (
       <div className="App">
@@ -17,5 +29,17 @@ class App extends Component {
     );
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
 
 export default App;
